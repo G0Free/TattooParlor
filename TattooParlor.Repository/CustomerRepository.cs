ed156aa3dc50f6e3fdb20f3ef3f6fc6ae71b4280
G0Free <gyorfi.bence@gmail.com>
2022-01-12 20:54:24 +0100

    CustomerRepository done

--- TattooParlor.Repository/CustomerRepository.cs
+++ TattooParlor.Repository/CustomerRepository.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TattooParlor.Models;
 
 namespace TattooParlor.Repository
 {
-    class CustomerRepository : Repository<>
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
     {
+        public CustomerRepository(DbContext ctx) : base(ctx)
+        {
+            //done
+        }
+
+        public void AddNewCustomer(Customer customer)
+        {
+            ctx.Add(customer);
+            ctx.SaveChanges();
+        }
+
+        public void ChangeBirthYear(int id, int newBirthYear)
+        {
+            var customer = GetOne(id);
+            customer.BirthYear = newBirthYear;
+            ctx.SaveChanges();
+        }
+
+        public void ChangeEmail(int id, string newEmail)
+        {
+            var customer = GetOne(id);
+            customer.Email = newEmail;
+            ctx.SaveChanges();
+        }
+
+        public void ChangeFirstName(int id, string newFirstName)
+        {
+            var customer = GetOne(id);
+            customer.FirstName = newFirstName;
+            ctx.SaveChanges();
+        }
+
+        public void ChangeLastName(int id, string newLastName)
+        {
+            var customer = GetOne(id);
+            customer.LastName = newLastName;
+            ctx.SaveChanges();
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            var toDelete = GetOne(id);
+            ctx.Remove(toDelete);
+            ctx.SaveChanges();
+        }
+
+        public override Customer GetOne(int id)
+        {
+            return GetAll().FirstOrDefault(x => x.CustomerId == id);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            var toUpdate = GetOne(customer.CustomerId);
+            toUpdate.FirstName = customer.FirstName;
+            toUpdate.LastName = customer.LastName;
+            toUpdate.BirthYear = customer.BirthYear;
+            toUpdate.Email = customer.Email;
+            ctx.SaveChanges();
+        }
     }
 }
