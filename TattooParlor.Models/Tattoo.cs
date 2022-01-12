7de42a0b23512897d357f76474ee2aa3a628987b
G0Free <gyorfi.bence@gmail.com>
2022-01-12 19:10:20 +0100

    Properties added

---
+++ TattooParlor.Models/Tattoo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TattooParlor.Models
+{
+    public class Tattoo
+    {
+        public int TattoId { get; set; }
+        public string FantasyName { get; set; }
+    }
+}
