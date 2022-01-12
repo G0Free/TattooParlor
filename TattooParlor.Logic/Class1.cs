c7da777076b3e45398079179162a625bb7519c2c
G0Free <gyorfi.bence@gmail.com>
2022-01-12 21:13:44 +0100

    Interface added

--- TattooParlor.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TattooParlor.Logic
-{
-    public class Class1
-    {
-    }
-}
---
+++ TattooParlor.Logic/ICustomerLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TattooParlor.Models;
+
+namespace TattooParlor.Logic
+{
+    public interface ICustomerLogic
+    {
+        //Create
+        void AddNewCustomer(Customer customer);
+
+        //Read
+        Customer GetCustomerById(int id);
+
+        //ReadAll
+        IEnumerable<Customer> GetAllCustomers();
+
+        //Update
+        void ChangeBirthYear(int id, int newBirthYear);
+        void ChangeEmail(int id, string newEmail);
+        void ChangeFirstName(int id, string newFirstName);
+        void ChangeLastName(int id, string newLastName);
+
+        //Delete
+        void DeleteCustomer(int id);
+    }
+}
