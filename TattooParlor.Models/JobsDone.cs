6cf40244da8c7647e91f4f36711ee49b16bbfafd
G0Free <gyorfi.bence@gmail.com>
2022-01-12 19:08:31 +0100

    Properties added

---
+++ TattooParlor.Models/JobsDone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TattooParlor.Models
+{
+    public class JobsDone
+    {
+        public Customer customer { get; set; }
+        public Tattoo  tattoo{ get; set; }
+
+        public DateTime jobDate { get; set; }
+        public int Cost { get; set; }
+    }
+}
