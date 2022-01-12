0eca712085d3e4e616857e200e29bedc0628a796
G0Free <gyorfi.bence@gmail.com>
2022-01-12 21:05:58 +0100

    Repositories done

--- TattooParlor.Repository/JobsDoneRepository.cs
+++ TattooParlor.Repository/JobsDoneRepository.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TattooParlor.Models;
 
 namespace TattooParlor.Repository
 {
-    class JobsDoneRepository
+    class JobsDoneRepository : Repository<JobsDone>, IJobsDoneRepository
     {
+        public JobsDoneRepository(DbContext ctx): base(ctx)
+        {
+
+        }
+
+        public void AddNewJobsDone(JobsDone newJobsDone)
+        {
+            ctx.Add(newJobsDone);
+            ctx.SaveChanges();
+        }
+
+        public void ChangeCost(int id, int newCost)
+        {
+            var tattoo = GetOne(id);
+            tattoo.Cost = newCost;
+            ctx.SaveChanges();
+        }
+
+        public void ChangeJobDate(int id, DateTime newJobDate)
+        {
+            var tattoo = GetOne(id);
+            tattoo.jobDate = newJobDate;
+            ctx.SaveChanges();
+        }
+
+        public void DeleteJobsDone(int id)
+        {
+            var toDelete = GetOne(id);
+            ctx.Remove(toDelete);
+            ctx.SaveChanges();
+        }
+
+        public override JobsDone GetOne(int id)
+        {
+            return GetAll().FirstOrDefault(x => x.JobId == id);
+        }
+
+        public void UpdateJobsDone(JobsDone jobsDone)
+        {
+            var toUpdate = GetOne(jobsDone.JobId);
+            toUpdate.Cost = jobsDone.Cost;
+            toUpdate.jobDate = jobsDone.jobDate;
+            ctx.SaveChanges();
+        }
     }
 }
--- TattooParlor.Repository/TattooRepository.cs
+++ TattooParlor.Repository/TattooRepository.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TattooParlor.Models;
 
 namespace TattooParlor.Repository
 {
-    class TattooRepository
+    class TattooRepository : Repository<Tattoo>, ITattooRepository
     {
+        public TattooRepository(DbContext ctx) : base(ctx)
+        {
+
+        }
+        public void AddNewTatto(Tattoo tattoo)
+        {
+            ctx.Add(tattoo);
+            ctx.SaveChanges();
+        }
+
+        public void ChangeFantasyName(int id, string newName)
+        {
+            var tattoo = GetOne(id);
+            tattoo.FantasyName = newName;
+            ctx.SaveChanges();
+        }
+
+        public void DeleteTatto(int id)
+        {
+            var toDelete = GetOne(id);
+            ctx.Remove(toDelete);
+            ctx.SaveChanges();
+        }
+
+        public override Tattoo GetOne(int id)
+        {
+            return GetAll().FirstOrDefault(x => x.TattoId == id);
+        }
+
+        public void UpdateTatto(Tattoo tattoo)
+        {
+            var toUpdate = GetOne(tattoo.TattoId);
+            toUpdate.FantasyName = tattoo.FantasyName;
+
+            ctx.SaveChanges();
+        }
     }
 }
