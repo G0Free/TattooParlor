8e5511377c9436841166840ea6ea99509245fdc6
G0Free <gyorfi.bence@gmail.com>
2022-01-12 21:19:06 +0100

    Logic interface added

---
+++ TattooParlor.Logic/ITattooLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TattooParlor.Models;
+
+namespace TattooParlor.Logic
+{
+    public interface ITattooLogic
+    {
+        //Create
+        void AddNewTattoo(Tattoo tattoo);
+
+        //Read
+        Tattoo GetTattooById(int id);
+
+        //ReadAll
+        IEnumerable<Tattoo> GetAllTattoes();
+
+        //Update
+        void UpdateTattoo(Tattoo tattoo);
+        void ChangeFantasyName(int id, string newName);
+
+        //Delete
+        void DeleteTatto(int id);
+    }
+}
