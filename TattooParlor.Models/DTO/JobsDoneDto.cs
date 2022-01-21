7e9fa447af659b53e13562e230021087b5eb905d
G0Free <gyorfi.bence@gmail.com>
2022-01-21 15:33:42 +0100

    Data Transfer Objects done

---
+++ TattooParlor.Models/DTO/JobsDoneDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TattooParlor.Models.DTO
+{
+    public class JobsDoneDto
+    {
+        public int JobsDoneId { get; set; }
+        public int? customerId { get; set; }
+        public int? TattooId { get; set; }
+        public DateTime jobDate { get; set; }
+        public int Cost { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ TattooParlor.Models/DTO/TattooDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TattooParlor.Models.DTO
+{
+    public class TattooDto
+    {
+        public int TattoId { get; set; }
+        public string FantasyName { get; set; }
+        public int? jobsDoneId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
